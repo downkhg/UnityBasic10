9ff78ff86e33a58f032b31af60c0efdb7ee0a3ef
downkhg <downkhg@gmail.com>
2021-04-25 15:38:48 +0900

    아이템 획득하기

--- UnityPlatfomer/Assets/Scripts/Dynamic.cs
+++ UnityPlatfomer/Assets/Scripts/Dynamic.cs
@@ -7,6 +7,7 @@ public class Dynamic : MonoBehaviour
     public float Speed;
     public float JumpPower = 100;
     public bool isJump = false;
+    public int Score;
 
     // Start is called before the first frame update
     void Start()
@@ -27,7 +28,7 @@ public class Dynamic : MonoBehaviour
             if (isJump == false)
             {
                 Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
-                rigidbody.velocity = Vector2.zero;
+                rigidbody.velocity = Vector2.zero;//현재강체의 가속도를 0으로 만든다.
                 rigidbody.AddForce(Vector3.up * JumpPower);
 
                 isJump = true;
@@ -38,17 +39,31 @@ public class Dynamic : MonoBehaviour
     private void OnGUI()
     {
         GUI.Box(new Rect(0, 0, 100, 20), "isJump:" + isJump);
+        GUI.Box(new Rect(0, 20, 100, 20), "Score:" + Score);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        isJump = false;
-        Debug.Log("OnCollisionEnter2D:"+collision.gameObject.name);
+        //if (collision.gameObject.name == "Tilemap")
+        {
+            isJump = false;
+            Debug.Log("OnCollisionEnter2D:" + collision.gameObject.name);
+        }
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
         //isJump = true; //다른블럭에서 이동했을때도 이 경우에 해당이 된다.
         Debug.Log("OnCollisionExit2D:" + collision.gameObject.name);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //if (collision.gameObject.name == "cherry")
+        if (collision.gameObject.tag == "Item")
+        {
+            Score++;
+            Destroy(collision.gameObject);
+        }
     }
 }

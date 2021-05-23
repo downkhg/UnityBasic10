831d7993e6d55dc4e1d403f29243665df28270b6
downkhg <downkhg@gmail.com>
2021-05-23 16:18:59 +0900

    독수리 추가하기(콜라이더)

---
+++ UnityPlatfomer/Assets/Scripts/Eagle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eagle : MonoBehaviour
+{
+    public float Speed;
+    public GameObject objTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 vPos = this.transform.position;
+        Vector3 vTargetPos = objTarget.transform.position;
+        Vector3 vDist = vTargetPos - vPos;
+        Vector3 vDir = vDist.normalized;//방향
+        float fDist = vDist.magnitude; //거리
+
+        if(fDist > Time.deltaTime)//독수리가 일정거리까지 가기전까지만 
+            transform.position += vDir * Speed * Time.deltaTime; //이동
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+            objTarget = collision.gameObject;
+    }
+}
